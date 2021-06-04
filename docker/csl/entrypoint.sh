b47dd9a3fda1b45e85b03e025b838392a9ece9f2
Jimmy Chen <jimmy.chen@hardcoretech.co>
2021-06-04 19:41:57 +0800

    ci(csl entrypoint): Add RECREATE_DB environment variables.
    [why] we want container default command can be changed by environment variables.
    [how] by adding RECREATE_DB into entrypoint script.

--- docker/csl/entrypoint.sh
+++ docker/csl/entrypoint.sh
@@ -2,8 +2,9 @@
 
 /usr/bin/wait-for-it.sh elastic:9200 -t 30  -- echo "Elasticsearch server is ready"
 
-bundle exec rake db:create
-
-/usr/bin/import.sh
+if [ "$RECREATE_DB" = "true" ] ; then
+    bundle exec rake db:create
+    /usr/bin/import.sh
+fi
 
 bundle exec rails s -b 0.0.0.0
