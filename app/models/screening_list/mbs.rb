deff8251c46e6bbf58311fab12b58f37ef9a956a
daniel <daniel@hardcoretech.co>
2021-07-01 17:07:54 +0800

    feat(csl): add mbs source
    GFG-10793

---
+++ app/models/screening_list/mbs.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module ScreeningList
+  class Mbs
+    include Indexable
+    include ScreeningList::Mappable
+    self.source = {
+      full_name: "Non-SDN Menu-Based Sanctions List (NS-MBS List) - Treasury Department",
+      code:      "MBS",
+    }
+  end
+end
